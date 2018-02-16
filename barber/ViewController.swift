03b457a3296c4e99d432021dfdca788787596584
amr sobhy <amr.geek.95@gmail.com>
2018-02-16 02:11:46 +0200

    Initial Commit

---
+++ barber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  barber
+//
+//  Created by amr sobhy on 2/16/18.
+//  Copyright © 2018 amr sobhy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
